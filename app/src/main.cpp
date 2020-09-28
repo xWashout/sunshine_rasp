7401a480a23cdeffc990e68ff613d12e39d131eb
xWashout <washouter@gmail.com>
2020-09-28 23:28:28 +0200

    draft

---
+++ app/src/main.cpp
@@ -0,0 +1,8 @@
+#include <mqtt_wrapper.h>
+
+int main(int argc, char *argv[])
+{
+    MqttWrapper mqttWrapper;
+    mqttWrapper.Publisher();
+    return 0;
+}
---
+++ mqtt/inc/mqtt_wrapper.h
@@ -0,0 +1,12 @@
+#ifndef MQTTWRAPPER_H
+#define MQTTWRAPPER_H
+
+
+class MqttWrapper
+{
+public:
+    MqttWrapper();
+    int Publisher();
+};
+
+#endif // MQTTWRAPPER_H
