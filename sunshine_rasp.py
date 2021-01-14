7937c59ae47b861e35a4269dbccc3ce9d7f4b443
xWashout <washouter@gmail.com>
2021-01-14 02:01:59 +0100

    timers init

--- sunshine_rasp.py
+++ sunshine_rasp.py
@@ -1,7 +1,5 @@
 import paho.mqtt.client as mqtt
-import threading
 import time
-
 from threading import Thread, Lock
 from ccs811 import *
 from hdc2010 import *
@@ -10,11 +8,11 @@ mutex = Lock()
 broker_address="broker.hivemq.com"
 port = 1883
 
-## Measurement Frequency (seconds) default = 10s ##
-measFreqTemp = 3
-measFreqHum = 5
-measFreqTvoc = 7
-measFreqCo2 = 11
+## Default Measurements Frequency (seconds) ##
+measFreqTemp = 10
+measFreqHum = 20
+measFreqTvoc = 30
+measFreqCo2 = 40
 
 ## COMMON FOR MQTT ##
 def onMessage(client, userdata, message):
@@ -27,10 +25,11 @@ def onMessage(client, userdata, message):
 
 def onConnect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
     print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
-    client.subscribe("rasp3bFreqTemp") 
-    client.subscribe("rasp3bFreqHum") 
-    client.subscribe("rasp3bFreqTvoc") 
-    client.subscribe("rasp3bFreqCo2") 
+    client.subscribe("rasp3BFreqTemp") 
+    client.subscribe("rasp3BFreqHum") 
+    client.subscribe("rasp3BFreqTvoc") 
+    client.subscribe("rasp3BFreqCo2") 
+    client.subscribe("microcontrollerInit")
 
 def onLog(client, userdata, level, buf):
     print("log: ",buf)
@@ -48,23 +47,29 @@ def printCurrentFreqs():
 
 def topicVariableMapper(topic, value):
     mutex.acquire()
+    global measFreqTemp
+    global measFreqHum
+    global measFreqTvoc
+    global measFreqCo2
+    print("Receive data with Topic=", topic)
     try:
-        if topic == "rasp3bFreqTemp":
+        if topic == "rasp3BFreqTemp":
             print(value)
-            global measFreqTemp
             measFreqTemp = int(value)
-        elif topic == "rasp3bFreqHum":
+        elif topic == "rasp3BFreqHum":
             print(value)
-            global measFreqHum
             measFreqHum = int(value)
-        elif topic == "rasp3bFreqTvoc":
+        elif topic == "rasp3BFreqTvoc":
             print(value)
-            global measFreqTvoc
             measFreqTvoc = int(value)
-        elif topic == "rasp3bFreqCo2":
-            global measFreqCo2
+        elif topic == "rasp3BFreqCo2":
             measFreqCo2 = int(value)
             print(value)
+        elif topic == "microcontrollerInit":
+            client.publish("rasp3BFreqTemp", measFreqTemp)
+            client.publish("rasp3BFreqHum", measFreqHum)
+            client.publish("rasp3BFreqTvoc", measFreqTvoc)
+            client.publish("rasp3BFreqCo2", measFreqCo2)
         else:
             print("Invalid topic")
     finally:
@@ -73,8 +78,8 @@ def topicVariableMapper(topic, value):
 def sendTemperature():
     while True:
         temp = hdc2010ReadTemp()
-        #if temp > 0:
-        client.publish("rasp3b_temperature", temp)
+        if temp > 0:
+            client.publish("rasp3BTemperature", temp)
 
         mutex.acquire()
         try:
@@ -89,8 +94,8 @@ def sendTemperature():
 def sendHumidity():
     while True:
         hum = hdc2010ReadHumidity()
-        #if hum > 0:
-        client.publish("rasp3b_humidity", hum)
+        if hum > 0:
+            client.publish("rasp3BHumidity", hum)
 
         mutex.acquire()
         try:
@@ -106,8 +111,8 @@ def sendTvoc():
     while True:
         if ccs811CheckDataAndUpdate():
             tvoc = ccs811GetTVOC()
-            #if tvoc > 0:
-            client.publish("rasp3b_tvoc", tvoc)
+            if tvoc > 0:
+                client.publish("rasp3BTvoc", tvoc)
         elif ccs811CheckForError():
             ccs811PrintError()
 
@@ -125,8 +130,8 @@ def sendCo2():
     while True:
         if ccs811CheckDataAndUpdate():
             co2 = ccs811GetCO2()
-            #if co2 > 0:
-            client.publish("rasp3b_co2", co2)
+            if co2 > 0:
+                client.publish("rasp3BCo2", co2)
         elif ccs811CheckForError():
             ccs811PrintError()
 
@@ -162,10 +167,10 @@ client.connect(broker_address, port)
 client.loop_start()
 
 ## THREADS INIT ##
-tempSenderThread = threading.Thread(target=sendTemperature)
-humSenderThread = threading.Thread(target=sendHumidity)
-tvocSenderThread = threading.Thread(target=sendTvoc)
-co2SenderThread = threading.Thread(target=sendCo2)
+tempSenderThread = Thread(target=sendTemperature)
+humSenderThread = Thread(target=sendHumidity)
+tvocSenderThread = Thread(target=sendTvoc)
+co2SenderThread = Thread(target=sendCo2)
 
 tempSenderThread.daemon = True
 humSenderThread.daemon = True
